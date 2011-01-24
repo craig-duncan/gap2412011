6113d50eb72da9cb39810e63159ae3ae58b78701
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-01-24 10:56:52 -0500

    Updated the script

--- start_new_exercise.ps1
+++ start_new_exercise.ps1
@@ -1,20 +1,7 @@
 param($message)
 
-function get_commit_message($passed_in_at_command_line)
-{
-  $commit_message = $passed_in_at_command_line
-
-  if ($commit_message -eq $null)
-  {
-    write-host "Please enter a commit message (leave blank to exit):"
-    $commit_message = read-host
-  }
-  return $commit_message
-}
-
 function format_time_as_new_branch_name
 {
-  "$($now.year)$($now.month)$($now.day)$($now.hour)$($now.minute)"
 }
 
 function get_latest_on_new_branch($branch_name)
@@ -26,6 +13,8 @@ function get_latest_on_new_branch($branch_name)
   git pull jp master
 }
 
-$new_branch_name = format_time_as_new_branch_name
+$now = get-date
+$new_branch_name = "$($now.year)$($now.month)$($now.day)$($now.hour)$($now.minute)"
+echo "new branch name:$new_branch_name message:$commit_message"
 get_latest_on_new_branch($new_branch_name)
 echo "new branch name:$new_branch_name message:$commit_message"
