0dda77f4e518f2f8aa979fa4363b7cd9c46467d8
prasad narravula <pnarravulla@invisioninc.com>
2011-01-24 16:46:35 -0500

    Updated

--- start_new_exercise.ps1
+++ start_new_exercise.ps1
@@ -1,33 +1,23 @@
 param($message)
 
-function get_commit_message($passed_in_at_command_line)
-{
-  $commit_message = $passed_in_at_command_line
-
-  if ($commit_message -eq $null)
-  {
-    write-host "Please enter a commit message (leave blank to exit):"
-    $commit_message = read-host
-  }
-  return $commit_message
-}
-
 function format_time_as_new_branch_name
 {
-  "$($now.year)$($now.month)$($now.day)$($now.hour)$($now.minute)"
 }
 
-function get_latest_on_new_branch($branch_name,$commit_message)
+function get_latest_on_new_branch($branch_name)
 {
   git add -A
-  git commit -m $commit_message
+  git commit -m "Committing"
   git checkout master
-  git checkout -b $branch_name
+  git checkout -b format_time_as_new_branch_name
   git pull jp master
 }
 
-$commit_message = get_commit_message($message)
-if ($commit_message -eq [String]::empty) {exit}
-$new_branch_name = format_time_as_new_branch_name
-get_latest_on_new_branch($new_branch_name,$commit_message)
-echo "new branch name:$new_branch_name message:$commit_message"
+$now = get-date
+$new_branch_name = "$($now.year)$($now.month)$($now.day)$($now.hour)$($now.minute)"
+git add -A
+git commit -m "Committing"
+git checkout master
+git checkout -b $new_branch_name
+git pull jp master
+echo "new branch name:$new_branch_name"
